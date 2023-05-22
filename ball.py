2b57c27a69d7a12e6f895a95eb9b46cb51995660
lukestroh <66746219+lukestroh@users.noreply.github.com>
2023-05-21 18:13:33 -0700

    deleted some old functions

--- ball.py
+++ ball.py
@@ -26,34 +26,6 @@ class Ball():
         self.g = sc.g
         self.dt = dt
         return
-    
-    def get_position(self, t, drone_coordinates):
-        """
-        Step iteration of the ball falling in space
-        """
-        # start at some initial point with initial velocity values of 0
-        # have ball fall bc gravity
-        # initial x is same for drone and ball so ball just falls straight downwards
-        
-        t += self.dt
-        self.x += self.vx * self.dt
-        self.y += self.vy * self.dt
-        self.vy -= self.g * self.dt
-
-        # This should maybe include the ball radius now?
-        if (
-            (drone_coordinates[1]-0.1) < self.y < drone_coordinates[1]
-        ) and (
-            (self.drone_coordinates[0]-0.25) <= self.x <= (self.drone_coordinates[0]+0.25)
-        ):
-            self.y = self.drone_coordinates[1]
-            self.vy = 0
-        elif (self.y <= 0):
-            self.y = 0
-            self.vy = 0
-            self.vx = 0
-
-        yield self.x, self.y
 
     def step(self, drone_state = None):
         if drone_state:
